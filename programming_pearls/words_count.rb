0bbc30204a0ab476bfc150fb1801ea6d40dd4e55
Ethan Yang <ethan.y@outlook.com>
2015-08-24 16:16:35 +1000

    Refactoring

--- programming_pearls/words_count.rb
+++ programming_pearls/words_count.rb
@@ -2,16 +2,21 @@ unless ARGV.size == 1
   abort 'format: words_count [input filename]'
 end
 
+def count_word(map, word)
+  map[word] = 0 unless map[word]
+  map[word] += 1
+end
+
+def count_line_of_words(map, line)
+  words = line.split /\s+/
+  words.each { |word| count_word map, word }
+end
+
 input_filename = ARGV[0]
 
 map = {}
 
-File.open(input_filename).each_line do |line|
-  words = line.split /\s+/
-  words.each do |word|
-    map[word] = 0 unless map[word]
-    map[word] += 1
-  end
-end
+File.open(input_filename).each_line { |line| count_line_of_words map, line }
 
 puts map
+
