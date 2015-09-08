188c83bafebc017a510aeb23f6bd8336318ccde0
Ethan Yang <ethan.y@outlook.com>
2015-09-08 05:04:16 -0700

    Using pointer return

--- src/strcpy.c
+++ src/strcpy.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char *mystrcpy(char *dest, char *src) {
+  char *temp = dest;
   while(*dest++ = *src++);
-  return dest;
+  return temp;
 }
 
 int main(){
   char src[] = "a string.";
   char str[256];
-  mystrcpy(str, src);
+  char *str1 = malloc(256);
+  str1 = mystrcpy(str, src);
   printf("%s\n", str);
+  printf("%s\n", str1);
   return 0;
 }
